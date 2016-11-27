ae70424873b4a3683b14350e0ead0b21ffb47f6f
Hal Chen <yalahao@gmail.com>
2016-11-27 10:04:05 +0100

    Added links between rango index and about

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Rango says hey there partner")
+  return HttpResponse("Rango says hey there partner. <br/> <a href='/rango/about'>About page</a>")
+
+def about(request):
+  return HttpResponse("Rango loves this about page. <br/> <a href='/rango/'>Back to index</a>")
