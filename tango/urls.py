0a3b7bb6072636579c606b196993e47c676fa174
Hal Chen <yalahao@gmail.com>
2016-11-27 09:56:18 +0100

    Modularised rango urls

--- tango/urls.py
+++ tango/urls.py
@@ -15,14 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
-
+from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-  url(r'^$', views.index, name='index'),
-  url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
 ]
